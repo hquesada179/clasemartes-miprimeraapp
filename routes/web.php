e28137526f8a2b5cf06a0887fac4abf878d3528a
hquesada179 <hquesada179@unab.edu.co>
2026-02-10 15:34:23 -0500

    mejore el error de php que tenia

--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,76 @@
 <?php
 
-use Illuminate\HTTP\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-
-Route::get('/contacto'), function(Request $request) {
-    print _r($request);
-    echo "</pre>";
-
-
-}
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+
+Route::get('/contacto', function (Request $request) {
+    echo "<pre>";
+    print_r($request->all()); // muestra datos (query, form, etc.)
+    echo "</pre>";
+
+    return "Ruta contacto OK";
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/mi-ruta-a', function () {
-    return ('Hola este es mi ruta');
+    return 'Hola este es mi ruta';
 });
 
 Route::get('/php-basico', function () {
 
-     echo " <h1>Hola</h1>";
+    $nombre = "Helbert Mauricio";
+    $apellido = "Quesada Lopez";
+    $nombre_completo = $nombre . " " . $apellido;
 
-     $nombre = "Helbert Mauricio";
-     $apellido = "Quesada Lopez";
-     $nombre_completo = $nombre . $apellido;
-     $age = rand(10,54);
-     $height = 1.75;
-     $isLogin = rand(0,1);
-     echo $nombre_completo;
+    $age = rand(10, 54);
+    $height = 1.75;
+    $isLogin = rand(0, 1);
 
-     echo "*****************ESTRUCTURA DE DATOS****************";
+    $message = "Soy $nombre_completo, tengo $age años";
 
-     $message = "soy $nombre_completo tengo $age años";
-     echo $message;
+    $productsNames = ["Computador", "Teclado", 25, true, false];
 
-
-     echo "**********FUNCIONES************";
-
-
-     $productsNames = ["Computador", "Teclado", 25, true, false];
-     $teclado = [
+    $teclado = [
         'name' => "teclado hp",
         'marca' => "HP",
-        'precio' =>20000,
+        'precio' => 20000,
         'distribuciones' => [
             'latino',
             'mexico',
             'americano'
         ]
-     ];
+    ];
 
-     $teclado['marca'] = "LG";
-     echo $teclado['name'];
+    $teclado['marca'] = "LG";
 
-     foreach ($productsNames as $item) {
-    echo $item . "<br>";
-}
+    // Construimos salida HTML (mejor que usar muchos echo)
+    $html  = "<h1>Hola</h1>";
+    $html .= "<p><strong>Nombre:</strong> $nombre_completo</p>";
+    $html .= "<p><strong>Mensaje:</strong> $message</p>";
+    $html .= "<hr>";
+    $html .= "<h3>Estructura de datos</h3>";
+    $html .= "<p><strong>Teclado:</strong> {$teclado['name']} ({$teclado['marca']})</p>";
+    $html .= "<p><strong>Productos:</strong></p><ul>";
 
+    foreach ($productsNames as $item) {
+        $html .= "<li>" . (is_bool($item) ? ($item ? 'true' : 'false') : $item) . "</li>";
+    }
 
+    $html .= "</ul>";
 
-
-
+    return $html;
 });
 
-function printUser( stirng $name, int $age){
+function printUser(string $name, int $age)
+{
     return "$name tiene $age años!!!";
 }
