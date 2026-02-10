9edcc088ebf125e7eea9764be790ffe4a6a8149e
hquesada179 <hquesada179@unab.edu.co>
2026-02-10 15:06:23 -0500

    cambie el web.php.

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,55 @@ Route::get('/', function () {
 });
 
 Route::get('/mi-ruta-a', function () {
-    return view('Hola este es mi ruta');
+    return ('Hola este es mi ruta');
 });
 
 Route::get('/php-basico', function () {
-    return view('Estoy aprendiendo php');
+
+     echo " <h1>Hola</h1>";
+
+     $nombre = "Helbert Mauricio";
+     $apellido = "Quesada Lopez";
+     $nombre_completo = $nombre . $apellido;
+     $age = rand(10,54);
+     $height = 1.75;
+     $isLogin = rand(0,1);
+     echo $nombre_completo;
+
+     echo "*****************ESTRUCTURA DE DATOS****************";
+
+     $message = "soy $nombre_completo tengo $age años";
+     echo $message;
+
+
+     echo "**********FUNCIONES************";
+
+
+     $productsNames = ["Computador", "Teclado", 25, true, false];
+     $teclado = [
+        'name' => "teclado hp",
+        'marca' => "HP",
+        'precio' =>20000,
+        'distribuciones' => [
+            'latino',
+            'mexico',
+            'americano'
+        ]
+     ];
+
+     $teclado['marca'] = "LG";
+     echo $teclado['name'];
+
+     foreach ($productsNames as $item) {
+    echo $item . "<br>";
+}
+
+
+
+
+
 });
+
+function printUser( stirng $name, int $age){
+    return "$name tiene $age años!!!";
+}
